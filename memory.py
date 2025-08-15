74f14d50cf3a503739ab3486513391bc8227c446
krishna-bansall <bansalkrishna321go@gmail.com>
2025-08-15 20:55:51 +0530

    Memrory added

---
+++ memory.py
@@ -0,0 +1,26 @@
+class Memory():
+    def __init__(self):
+        # data defined in bytes
+        self.memory = bytearray(4096)        
+        self.pointer = 0
+    
+    
+    # Methods of writing to the memory    
+    def write(self, address, buffer:bytearray):
+        # write the buffer to the memory at the address        
+        self.pointer = address
+        
+        for i, byte in enumerate(buffer):
+            self.memory[self.pointer + i] = byte
+            
+        self.pointer += len(buffer) 
+    
+    
+    # Methods to reading memory
+    def read_all(self):
+        print(self.memory)
+        
+    def show_mem(self):
+        for byte in self.memory:
+            print(byte)
+
---
+++ screen.py
@@ -0,0 +1,39 @@
+import pygame, math
+
+
+class Screen():
+    def __init__(self) -> None:
+        pygame.init()
+        self.screen = pygame.display.set_mode((64*10,32*10))
+        self.clock = pygame.time.Clock()
+        self.Running = True
+        self.dt=0 # delta time?  
+        self.frame = 0
+        self.pos = pygame.Vector2(0,0)
+
+    def run_screen_loop(self):
+        
+        print("Screen Module Attached!")
+        
+        while self.Running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    Running = False
+                    
+            self.screen.fill("yellow")
+            pygame.draw.circle(self.screen, "red", self.pos, 50 )
+            
+            keys = pygame.key.get_pressed()
+
+            self.pos.y -= math.sin(self.frame / 10)
+            self.pos.x += math.sin(self.frame / 10)
+
+            pygame.display.flip()
+            dt = self.clock.tick(60)
+            print(self.frame)
+            self.frame += 1            
+                
+
+
+
+# pygame.quit()
