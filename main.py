61f2d6b110c27477ff0c9d92f96540b72a59af58
krishna-bansall <bansalkrishna321go@gmail.com>
2025-11-21 14:34:40 +0530

    partial fetch loop made

--- main.py
+++ main.py
@@ -23,13 +23,26 @@ font = bytearray([
 ])
 
 keyboard = [49,50,51,52,113,119,101,114,97,115,100,102, 122,120,99,118]
+
+program_counter = 511
+
+registers = {}
+for i in range(15):
+    registers[format(i, '0x')] = ""
 class CHIP8():
     def __init__(self) -> None:
-        self.ram = Memory()
-        self.ram.write(0x050, font)
+        # 0 - 512 is reserved for interpreter, 512 - 4095 program data.
+        self.memory = bytearray(4096)
+        self.pointer = 0     
         self.screen = Screen()
-        print("Ram Memory Attached!")
         self.stack = []
+        
+    def mem_write (self, buffer, index):
+        self.pointer = index
+        for i, byte in enumerate(buffer):
+            self.memory[self.pointer + i] = byte
+            
+        self.pointer += len(buffer) 
         
     def stack_push (self, item):
         self.stack.insert(0, item)
@@ -43,6 +56,45 @@ chip = CHIP8()
 Running = True
 frame = 0
 
+chip.mem_write(font, 0)
+# print(chip.memory)
+# print(chip.memory[500])
+
+
+def decode(instruction):
+    n1 = instruction[0:1]
+    match n1.lower():
+        case '0':
+            # clear_screen()
+            print("case 0 trig")
+        case '1':
+            print("case 1 trig")
+        case '6':
+            print("case 6 trig")
+        case '7':
+            print("case 7 trig")
+        case 'a':
+            print("case A trig")
+        case 'd':
+            print("case D trig")
+    
+print("Chip8 emulator start")
+path = 'ibm.ch8'
+
+romobj = open(path, "rb")
+rom_arr = bytearray(romobj.read())
+chip.mem_write(rom_arr, 511)
+
+# for idx, byte in enumerate(chip.memory):
+#     if idx == 511: print("ROM STARTS HERE")
+#     print(hex(byte), end=' ')
+
+while Running:
+    current_instruction = format(chip.memory[program_counter], '02x') + format(chip.memory[program_counter + 1], '02x')
+    decode(current_instruction)
+    program_counter += 2
+
+    if program_counter >= 1000: Running = False
 
 # while Running:
 #     for event in pygame.event.get():
